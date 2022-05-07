85e7678f8026c16be00a64b034f0687eb0abfc6c
Traiano Giuseppe Welcome <traiano.welcome@accenture.com>
2022-05-07 15:59:21 +0800

    obtain mfa otp through pipeline

--- deployer-image/build.sh
+++ deployer-image/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#Simple build script to save typing:
+#Simple build script to create deployer image for Eqonex pipelines
 #
 #./build.sh <the semantic vesion id>
 
 build_version=$1
-docker build -t archbungle/jenkins:"jcasc-$build_version" .
-docker push archbungle/jenkins:"jcasc-$build_version"
+docker build -t archbungle/jenkins-eqonex:"jcasc-$build_version" .
+docker push archbungle/jenkins-eqonex:"jcasc-$build_version"
--- deployer-image/eqonex-loadengine.groovy
+++ deployer-image/eqonex-loadengine.groovy
@@ -5,39 +5,51 @@ pipelineJob("ragnarok") {
 script("""
 pipeline {
     agent any
+
+    parameters {
+      string(name: 'MFA_TOKEN', defaultValue: 'null', description: 'Enter MFA Token')
+    }
+    
     stages {
         stage("Testing") {
             steps {
-               sh '''
-               echo `env`
-               kubectl version --client=true
-               '''  
+                sh '''#!/bin/bash
+                set +x
+                git clone "https://traianowelcome:REDACTED@bitbucket.org/traianowelcome/trading-system-load-engine.git"
+                cd trading-system-load-engine/
+                source env.sh \${MFA_TOKEN}
+                pwd
+                ls -l
+                echo \$HOME
+                cd  ~/
+                pwd
+                '''
             }
         }
 
         stage("Deploying") {
             steps {
-               sh '''
-               for i in `seq 1 20`;do echo "\$i - waiting to get ready ..." - \$(sleep 1);done
+               sh '''#!/bin/bash
+               for i in `seq 1 5`;do echo "\$i - waiting to get ready ..." - \$(sleep 1);done
                '''
             }
         }
 
        stage("Configuring") {
             steps {
-               sh '''
+               sh '''#!/bin/bash
                kubectl get pods -o wide -n ragnarok
                kubectl get services -n ragnarok
-               for i in `seq 1 20`;do echo "\$i - waiting to get ready ..." - \$(sleep 1);done
+               for i in `seq 1 5`;do echo "\$i - waiting to get ready ..." - \$(sleep 1);done
                '''
             }
         }
 
         stage("Decommissioning") {
             steps {
-               sh '''
-               for i in `seq 1 30`;do echo "\$i - running stuff ..." - \$(sleep 5);done
-               for i in `seq 1 10`;do echo "\$i - waiting for shut down ..." - \$(sleep 1);done
+               sh '''#!/bin/bash
+               for i in `seq 1 5`;do echo "\$i - running stuff ..." - \$(sleep 5);done
+               for i in `seq 1 5`;do echo "\$i - waiting for shut down ..." - \$(sleep 1);done
                kubectl get pods -n ragnarok
                '''
             }
